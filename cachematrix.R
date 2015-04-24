04d3030f85ee66f0b35f7522383372735a060612
marthagariasg <marthagariasg@gmail.com>
2015-04-24 11:44:03 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,19 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse
+## where "mt" is a matrix
+## 
 makeCacheMatrix <- function(x = matrix()) {
-
+inversa <- NULL
+set <-function(x) {
+        mt<<-x;
+        inversa<<-null;
+}
+get<-function() return(mt);
+setinv<-function(inv) inversa<<-inv;
+getinv<-function() return(inversa);
+return(list(set=set,get=get,setinv=setinv,getinv=getinv))
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inversa <- mt$getinv() 
+    if(!is.null(inversa)) { 
+       message("leyendo memoria cache...") 
+       return(inversa) 
+   } 
+   datos <- mt$get() 
+   invsersa <- solve(datos, ...) 
+   mt$setinv(inversa) 
+   return(inversa)
 }
